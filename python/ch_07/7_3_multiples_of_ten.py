ca6b4de596e16ebc9287bdaa09105a8642dd91b5
Alexis Gonzalez <glep.chair319@passinbox.com>
2025-04-22 11:00:27 -0700

    Solutions to 7-1, 7-2, 7-3 problems

---
+++ python/ch_07/7_3_multiples_of_ten.py
@@ -0,0 +1,6 @@
+usr = int(input("Enter a number? "))
+
+if usr % 10 == 0:
+    print(f"The number entered {usr} is divisible by 10!")
+else:
+    print(f"The number entered {usr} is not divisible by 10!")
